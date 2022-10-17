24cf70ceac8df014234b23bd3f69d84757103411
Khalid CHAIBOUB <92036687+kh-chaiboub@users.noreply.github.com>
2022-10-17 21:24:13 +0100

    Initial commit

---
+++ product-service/src/test/java/com/khaliddev/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khaliddev.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
